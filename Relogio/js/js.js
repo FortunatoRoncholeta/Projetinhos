53774411000c06894e03c985f7d3babb1d0e3ec5
Fortunato Roncholeta <47647771+FortunatoRoncholeta@users.noreply.github.com>
2022-08-17 15:29:35 -0300

    relogio

---
+++ Relogio/js/js.js
@@ -0,0 +1,12 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+
+const relogio = setInterval(function time(){
+
+
+
+});
+
+    
