4b87774e0c2d34d0b213e15dba09880a0c39e92b
Fortunato Roncholeta <47647771+FortunatoRoncholeta@users.noreply.github.com>
2022-08-18 16:55:58 -0300

    finalizando brasilia

--- Relogio/js/js.js
+++ Relogio/js/js.js
@@ -2,14 +2,17 @@
 const horas = document.getElementById('hora');
 const minutos = document.getElementById('minuto');
 const segundos = document.getElementById('segundo');
+const dias =document.getElementById('dia');
+const meses =document.getElementById('mes');
+const anos =document.getElementById('ano');
 
 
 const relogio = setInterval(function time(){
     //pega a hora atual do sistema com a função getHours()
-    let dataAgora =new Date();
-    let hora = dataAgora.getHours();
-    let minuto = dataAgora.getMinutes();
-    let segundo = dataAgora.getSeconds();
+    let relogio =new Date();
+    let hora = relogio.getUTCHours();
+    let minuto = relogio.getMinutes();
+    let segundo = relogio.getSeconds();
    
     // verifica se o valor do horário é menor que 10 e adiciona o 0 na frente
     if (hora < 10)  hora = '0' + hora;
@@ -20,6 +23,30 @@ const relogio = setInterval(function time(){
     horas.textContent = hora;
     minutos.textContent = minuto;
     segundos.textContent = segundo;
+
+});
+
+
+const data = setInterval(function time(){
+
+    
+
+
+
+    //pega a hora atual do sistema com a função getHours()
+    let calendario =new Date();
+    let dia = calendario.getUTCDate();
+    let mes = calendario.getUTCMonth()+1;
+    let ano = calendario.getUTCFullYear();
+    
+    if (dia < 10)  dia = '0' + dia ;
+    if (mes < 10)  mes = '0' + mes ;
+    if (ano < 10)  ano = '0' + ano;
+
+    // atualiza o valor do horário
+    dias.textContent = dia + '/';
+    meses.textContent = mes + '/';
+    anos.textContent = ano;
 
 });
 
