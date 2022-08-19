969211357af87b5a5431354a50d33308ddb6c19c
Fortunato Roncholeta <47647771+FortunatoRoncholeta@users.noreply.github.com>
2022-08-19 16:36:09 -0300

    finalização do estilo

--- Relogio/js/js.js
+++ Relogio/js/js.js
@@ -10,9 +10,9 @@ const anos =document.getElementById('ano');
 const relogio = setInterval(function time(){
     //pega a hora atual do sistema com a função getHours()
     let relogio =new Date();
-    let hora = relogio.getUTCHours();
-    let minuto = relogio.getMinutes();
-    let segundo = relogio.getSeconds();
+    let hora = relogio.getUTCHours()-3;
+    let minuto = relogio.getUTCMinutes();
+    let segundo = relogio.getUTCSeconds();
    
     // verifica se o valor do horário é menor que 10 e adiciona o 0 na frente
     if (hora < 10)  hora = '0' + hora;
@@ -51,3 +51,100 @@ const data = setInterval(function time(){
 });
 
     
+//===========Relogio mundiais ===============
+
+    
+
+//===========Nova iorque ===============
+const horasNY = document.getElementById('horaNY');
+const minutosNY = document.getElementById('minutoNY');
+const segundosNY = document.getElementById('segundoNY');
+const diasNY =document.getElementById('diaNY');
+const mesesNY =document.getElementById('mesNY');
+const anosNY =document.getElementById('anoNY');
+
+
+const relogioNY = setInterval(function time(){
+    //pega a hora atual do sistema com a função getHours()
+    let relogio =new Date();
+    let hora = relogio.getUTCHours()-4;
+    let minuto = relogio.getUTCMinutes();
+    let segundo = relogio.getUTCSeconds();
+   
+    // verifica se o valor do horário é menor que 10 e adiciona o 0 na frente
+    if (hora < 10)  hora = '0' + hora;
+    if (minuto < 10)  minuto = '0' + minuto;
+    if (segundo < 10)  segundo = '0' + segundo;
+    
+    // atualiza o valor do horário
+    horasNY.textContent = hora;
+    minutosNY.textContent = minuto;
+    segundosNY.textContent = segundo;
+
+});
+
+
+const dataNY = setInterval(function time(){
+    //pega a hora atual do sistema com a função getHours()
+    let calendario =new Date();
+    let dia = calendario.getUTCDate();
+    let mes = calendario.getUTCMonth()+1;
+    let ano = calendario.getUTCFullYear();
+    
+    if (dia < 10)  dia = '0' + dia ;
+    if (mes < 10)  mes = '0' + mes ;
+    if (ano < 10)  ano = '0' + ano;
+
+    // atualiza o valor do horário
+    diasNY.textContent = dia + '/';
+    mesesNY.textContent = mes + '/';
+    anosNY.textContent = ano;
+
+});
+
+//===========TOQUIO  ===============
+const horasJP = document.getElementById('horaJP');
+const minutosJP = document.getElementById('minutoJP');
+const segundosJP = document.getElementById('segundoJP');
+const diasJP =document.getElementById('diaJP');
+const mesesJP =document.getElementById('mesJP');
+const anosJP =document.getElementById('anoJP');
+
+
+const relogioJP = setInterval(function time(){
+    //pega a hora atual do sistema com a função getHours()
+    let relogio =new Date();
+    let hora = relogio.getUTCHours()-15;
+    let minuto = relogio.getUTCMinutes();
+    let segundo = relogio.getUTCSeconds();
+   
+    // verifica se o valor do horário é menor que 10 e adiciona o 0 na frente
+    if (hora < 10)  hora = '0' + hora;
+    if (minuto < 10)  minuto = '0' + minuto;
+    if (segundo < 10)  segundo = '0' + segundo;
+    
+    // atualiza o valor do horário
+    horasJP.textContent = hora;
+    minutosJP.textContent = minuto;
+    segundosJP.textContent = segundo;
+
+});
+
+
+const dataJP = setInterval(function time(){
+    //pega a hora atual do sistema com a função getHours()
+    let calendario =new Date();
+    let dia = calendario.getUTCDate()+1;
+    let mes = calendario.getUTCMonth()+1;
+    let ano = calendario.getUTCFullYear();
+    
+    if (dia < 10)  dia = '0' + dia ;
+    if (mes < 10)  mes = '0' + mes ;
+    if (ano < 10)  ano = '0' + ano;
+
+    // atualiza o valor do horário
+    diasJP.textContent = dia + '/';
+    mesesJP.textContent = mes + '/';
+    anosJP.textContent = ano;
+
+});
