4554ea9600484a66c86b5925ea93261ee5d43465
Fortunato Roncholeta <47647771+FortunatoRoncholeta@users.noreply.github.com>
2023-02-12 18:20:37 -0300

    finalizado

--- site Javascript/js.js
+++ site Javascript/js.js
@@ -34,13 +34,14 @@ function verificarano() {
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     var sexo = document.getElementsByName('radsex')
-    var botaogenero = document.getElementById('botaogenero').style.background = 'rgb(141, 230, 104)'
+
     // Elementbyname pega os elementos começando no 0 (o primeiro ) ate o chega no ultino ( N)
     if (fano.value.length == 0 || fano.value > ano) {
         alert(`[ERROR] Verifique o ano digitado e tente novamente !!`)
 
     }
     else {
+        let botaogenero = document.getElementById('botao').style.background = 'rgb(141, 230, 104)'
         var idade = ano - Number(fano.value)
         // aqui criamos uma string vazia para receber o genero
         var genero = ''
@@ -102,3 +103,161 @@ function verificarano() {
 
 
 }
+
+
+function contanumero() {
+
+    var txtinicio = document.getElementById('txtinicio')
+    var txtfim = document.getElementById('txtfim')
+    var txtpasso = document.getElementById('txtpasso')
+    let resultadocon = document.getElementById('resultadocon')
+    let inicio = Number(txtinicio.value)
+    let fim = Number(txtfim.value)
+    let passo = Number(txtpasso.value)
+
+
+    if (txtinicio.value.length == 0 || txtfim.value.length == 0 || txtpasso.value.length == 0) {
+        resultadocon.innerHTML = `impossivel contar`
+        alert('[ERROR] dados inconsistentes')
+    }
+
+    else {
+        let fundo = document.getElementById('contando').style.background = ('rgb(86, 215, 232)')
+
+
+        resultadocon.innerHTML = `Contando... <br> `
+        if (passo <= 0) {
+            alert('Passo invalido')
+        }
+
+        if (inicio < fim) {
+            //crescente
+            for (let i = inicio; i <= fim; i += passo) {
+                resultadocon.innerHTML += `${i} \u{1F427}`
+            }
+        }
+        else {
+            //regrssivo
+            for (let i = inicio; i >= fim; i -= passo) {
+
+                resultadocon.innerHTML += `${i} \u{1F427}`
+            }
+
+        }
+        resultadocon.innerHTML += `\u{1F3C1}`
+    }
+}
+
+function multiplicar() {
+
+    let numero = document.getElementById('txtmulti')
+    let res = document.getElementById('resultadotab')
+
+    //.value pra pegar o valor numerico 
+
+    if (numero.value.length == 0) {
+        alert('[ERRO] Número incorreto!!!')
+    }
+
+    else {
+        num = Number(numero.value)
+        let mult
+        res.innerHTML = ''
+        for (let i = 0; i <= 10; i++) {
+
+            mult = i * num
+            let item = document.createElement('option')
+            let fundo = document.getElementById('tabuada').style.background = 'rgb(240, 151, 151)'
+            item.text = `${i}X${num}=${mult}`
+            res.appendChild(item)
+
+        }
+    }
+
+}
+
+//----------Funçoes para o analisador de notas 
+let notas = []
+function verifcaNota(nota) {
+    // verificando se é vazio ou se é maior que 10 ou menor que 0
+    if (nota.value.length == 0 || nota.value < 0 || nota.value > 10) return -1
+}
+
+
+
+// verifica se o numero ja esta na lista
+function adicionar() {
+    let nota = document.getElementById('txtnotas')
+    let tabela = document.getElementById('tabnota')
+    let resultado = document.getElementById('resultadoNotas').innerHTML = ``
+
+
+
+
+    if (verifcaNota(nota) == -1) {
+        alert('[ERROR] Número invalido,Somente notas entre 0 e 10 são aceitas')
+    }
+
+
+    else {
+        let opt = document.getElementById('opt').innerHTML = `Notas:`
+        //adiciona no array o valor da nota
+        notas.push(Number(nota.value))
+        let lista = document.createElement('option')
+        lista.text = `Nota ${nota.value}`
+        tabela.appendChild(lista)
+
+
+    }
+
+    nota.value = ''
+    nota.focus()
+
+
+
+}
+
+function finalizar() {
+
+    let resultado = document.getElementById('resultadoNotas')
+    if (notas.length == 0 || notas.length == 1) alert('[ERROR] Sua lista esta vazia ou possui apenas uma nota')
+
+    else {
+        let auxMaior = notas[0]
+        let auxMenor = notas[0]
+        let Soma = 0
+        let Media = 0
+
+
+        for (let i in notas) {
+
+            Soma += notas[i]
+            Media = Soma / notas.length
+            if (notas[i] > auxMaior) {
+                auxMaior = notas[i]
+
+            }
+
+            if (notas[i] < auxMenor) {
+                auxMenor = notas[i]
+            }
+
+
+
+
+
+        }
+        resultado.innerHTML = `Foram inseridas ${notas.length} Notas. <br> <br>`
+        resultado.innerHTML += `O maior valor é ${auxMaior} <br> <br>`
+        resultado.innerHTML += `O menor valor é ${auxMenor} <br> <br>`
+        resultado.innerHTML += `A soma de todas as notas foi ${Soma} <br> <br>`
+        resultado.innerHTML += `A media da Turma foi de  ${Media} <br> <br>`
+
+
+
+    }
+
+
+}
+
+//-----------------------
