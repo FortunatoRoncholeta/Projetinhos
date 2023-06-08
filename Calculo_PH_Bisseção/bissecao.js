7ad70318d63cb5c5993d08551289778c188f6a59
Fortunato Roncholeta <47647771+FortunatoRoncholeta@users.noreply.github.com>
2023-06-08 14:56:57 -0300

    Cauculo Bessecao

---
+++ Calculo_PH_Bisseção/bissecao.js
@@ -0,0 +1,37 @@
+function Calcula(){
+
+
+function equacao_equilibrio_acido_base(pH) {
+    // Equação de equilíbrio ácido-base que relaciona o pH com as concentrações de H+ e OH-
+    return Math.pow(10, -pH) - Math.pow(10, -14 + pH);
+  }
+  
+  function bissecao(f, a, b, tolerancia) {
+    while (Math.abs(b - a) > tolerancia) {
+      let c = (a + b) / 2;
+      if (f(c) === 0) {
+        return c;
+      } else if (f(a) * f(c) < 0) {
+        b = c;
+      } else {
+        a = c;
+      }
+    }
+    return (a + b) / 2;
+  }
+  
+  // Definindo os valores iniciais
+  
+  
+  
+  const a= parseInt(document.getElementById('iniph').value)
+  const b = parseInt(document.getElementById('fimph').value)
+  const tolerancia=parseFloat(document.getElementById('Tolerancia').value)
+  
+  
+  
+  // Chamando a função de bisseção para encontrar o valor do pH
+  let pH = bissecao(equacao_equilibrio_acido_base, a, b, tolerancia);
+  
+  console.log("O pH da solução é:", pH);
+}
